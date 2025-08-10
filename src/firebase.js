84ba0451379976b4bd0392a233fdb8756c449332
Alsanmwn <nrmwnalsa@gmail.com>
2025-08-11 04:58:53 +0800

    ada perubahan

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-}
+  apiKey: "AIzaSyDbCadBhVkVoloOhUKsNFQCkueXnz4-GpY",
+  authDomain: "kknt-114-desatalawe.firebaseapp.com",
+  projectId: "kknt-114-desatalawe",
+  storageBucket: "kknt-114-desatalawe.firebasestorage.app",
+  messagingSenderId: "495642501138",
+  appId: "1:495642501138:web:c7e48bf9aac708e15938cb",
+  measurementId: "G-BTJ6ZPWD33"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
